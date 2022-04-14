70d4720bc3d36e8f7ca97c2b1530f8892c323879
alvaldes <angelluis2605@gmail.com>
2022-04-14 16:27:28 -0400

    Modify Section component to include id property

--- components/section.js
+++ components/section.js
@@ -7,12 +7,13 @@ const StyleDiv = chakra(motion.div, {
   }
 })
 
-const Section = ({ children, delay = 0 }) => (
+const Section = ({ children, delay = 0, id }) => (
   <StyleDiv
     initial={{ y: 10, opacity: 0 }}
     animate={{ y: 0, opacity: 1 }}
     transition={{ duration: 0.8, delay }}
     mb={6}
+    id={id}
   >
     {children}
   </StyleDiv>
